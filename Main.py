b13250de7717245f7fad1b209ba86607368b67c1
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-03-07 11:41:15 +1100

    made stuff work

--- Main.py
+++ Main.py
@@ -1,8 +1,16 @@
 # The main source of code and file
-def welcome_to_awesome_quiz():
+def welcome_to_awesome_quiz(): # Introduces player gives a short intro
     print("Welcome sir the most fantastic amazing quiz you will ever participate")
     print("This quiz will have three categories, how great is that")
 
 
-def main (): # Sir won't let the large 1000 line code happen :( wants it to be neat
-    welcome_to_awesome_quiz()
+def main(): # Sir won't let the large 1000 line code happen :( wants it to be neat
+    welcome_to_awesome_quiz() # Calls the welcome function to introduce player etc
+
+
+
+
+
+
+
+main() # Calls the final main program and makes stuff happen
