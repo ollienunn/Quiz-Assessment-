c9b079f5aff6c010c5149358eaa45d9f7c08b442
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-03-07 12:06:57 +1100

    Made a new file "Questions" was able to import the question from that file to main, Able to pinpoint specific parts of it like topic and question

--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
+from Questions import questions # Gets the file called questions than imports the dictionary called questions, very cool, very useful
+
 # The main source of code and file
 def welcome_to_awesome_quiz(): # Introduces player gives a short intro
     print("Welcome sir the most fantastic amazing quiz you will ever participate")
     print("This quiz will have three categories, how great is that")
 
+def question_getter(Questions): # Grabbing the questions for the quiz
+    for questions in Questions:
+        print(f"The topic is {questions['topic']}")
 
-def main(): # Sir won't let the large 1000 line code happen :( wants it to be neat
+def main(): # Sir won't let the large 1000 line code happen :( wants it to be neat, make it condensed into large function
     welcome_to_awesome_quiz() # Calls the welcome function to introduce player etc
+    question_getter(questions)
 
 
 
---
+++ Questions.py
@@ -0,0 +1,6 @@
+questions = [
+    {"topic": "General", "question": "What do you call a drunken salor?", "answer": "Dead weight"},
+    {"topic": "General", "question": "What do you call a drnken salor?", "answer": "Dead weight"},
+    {"topic": "General", "question": "What do you call a dunken salor?", "answer": "Dead weight"},
+    {"topic": "General", "question": "What do you call a drunkn salor?", "answer": "Dead weight"},
+]
