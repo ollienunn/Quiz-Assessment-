67161c80a322ca90250cde0563c74a593768ed1c
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-03-07 11:38:07 +1100

    Made a short intro

---
+++ Main.py
@@ -0,0 +1,8 @@
+# The main source of code and file
+def welcome_to_awesome_quiz():
+    print("Welcome sir the most fantastic amazing quiz you will ever participate")
+    print("This quiz will have three categories, how great is that")
+
+
+def main (): # Sir won't let the large 1000 line code happen :( wants it to be neat
+    welcome_to_awesome_quiz()
