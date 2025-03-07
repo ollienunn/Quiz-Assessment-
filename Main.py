13556c8f64ce79a7dc5c87104148257fbef7823a
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-03-07 12:27:21 +1100

    re arranged it to look nicer and work

--- Main.py
+++ Main.py
@@ -1,14 +1,6 @@
 from Questions import questions # Gets the file called questions than imports the dictionary called questions, very cool, very useful
 import sys # Random stuff to make typewritter effect work
 from time import sleep # Kinda understand this 
-# The main source of code and file
-def welcome_to_awesome_quiz(): # Introduces player gives a short intro
-    print("Welcome sir the most fantastic amazing quiz you will ever participate")
-    print("This quiz will have three categories, how great is that")
-
-def question_getter(Questions): # Grabbing the questions for the quiz
-    for questions in Questions: # Loops through the dictionary on other file for the stuff in questions
-        print(f"The topic is {questions['topic']}")# Just a test not definate
 
 def typewritter(words): # Typewritter effect to make the quiz look less boring
     for char in words:
@@ -20,6 +12,16 @@ def typewritter(words): # Typewritter effect to make the quiz look less boring
     sys.stdout.write("\n") # Adds a gap between the next line
 
 #typewritter("Youe iwehfowihf.") # Have to start with 'typewritter' for it to work
+
+def welcome_to_awesome_quiz(): # Introduces player gives a short intro
+    typewritter("Welcome sir the most fantastic amazing quiz you will ever participate")
+    typewritter("This quiz will have three categories, how great is that")
+
+def question_getter(Questions): # Grabbing the questions for the quiz
+    for questions in Questions: # Loops through the dictionary on other file for the stuff in questions
+        print(f"The topic is {questions['topic']}")# Just a test not definate
+
+
 
 def main(): # Sir won't let the large 1000 line code happen :( wants it to be neat, make it condensed into large function
     welcome_to_awesome_quiz() # Calls the welcome function to introduce player etc
