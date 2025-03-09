d2ba6ccb4602a048c457bce2d05320c0f9959e3a
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-03-09 11:17:35 +1100

    Made topic selesctor work by using a while loop and added further comments to explain

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Questions import questions # Gets the file called questions than imports the dictionary called questions, very cool, very useful
 import sys # Random stuff to make typewritter effect work
 from time import sleep # Kinda understand this 
+#import random # To mix up questions WEP
 
 def typewritter(words): # Typewritter effect to make the quiz look less boring
     for char in words:
@@ -11,7 +12,7 @@ def typewritter(words): # Typewritter effect to make the quiz look less boring
             sleep(0.5)
     sys.stdout.write("\n") # Adds a gap between the next line
 
-#typewritter("Your gonna love quiz.") # Have to start with 'typewritter' for it to work
+#typewritter("Your gonna love this quiz.") # Have to start with 'typewritter' for it to work
 
 def welcome_to_awesome_quiz(): # Introduces player gives a short intro / a brief description of the categories
     typewritter("Welcome sir to the most fantastic, amazing, best crafted quiz you will ever have the joy of taking.")
@@ -19,31 +20,34 @@ def welcome_to_awesome_quiz(): # Introduces player gives a short intro / a brief
     typewritter("The categories are....")
     typewritter("General Knowledge, can you get 10 / 10?")
     typewritter("Sport, Hope you know lots of different sports.")
-    typewritter("Stuff that I like, you an get hints for this one. You will need it.")
+    typewritter("Other, it's like General Knowledge but has very random questions, so you get hints for this one. You will need it.")
 
 def topic_selector(): # Asks the user for which topic they should chose
     typewritter("Now lets challenge you, what topic would you like to choose?")
-    typewritter("General Knowledge, Sport or Other ")
-    decision = input("")
-    decision_real = decision.lower()
-    if decision_real == "general knowledge":
-        typewritter("Good choice on General Knowledge")
-    elif decision_real == "sport":
-        typewritter("So your sporty")
-    elif decision_real == "other":
-        typewritter("Hope your prepared")
-    else:
-        typewritter("Whoops not an answer try again.")
-        decision = input("")
+    valid_answer = False # Needed to start the loop
+    while valid_answer == False: # Simple loop to check whether the answer is one that is avaible otherwise restates the question
+        typewritter("General Knowledge, Sport or Other ")
+        decision = input("") # Gets the input from the player
+        decision_real = decision.lower() # Makes it all lowercase so any way to type it will work
+        if decision_real == "general knowledge": # Must be all lower case
+            valid_answer = True # Ends the loop as it is an answer available
+            typewritter("Good choice on General Knowledge")
+        elif decision_real == "sport":
+            valid_answer = True
+            typewritter("So your sporty")
+        elif decision_real == "other":
+            valid_answer = True
+            typewritter("Hope your prepared")
+        else:
+            print("Please answer carefully")
 
 def question_getter(Questions): # Grabbing the questions for the quiz # Need to change it 
     for questions in Questions: # Loops through the dictionary on other file for the stuff in questions
         typewritter(f"The topic is {questions['topic']}")# Just a test not definate
 
 def main(): # Sir won't let the large 1000 line code happen :( wants it to be neat, make it condensed into large function
-    topic_selector() # So sir can fix the fuck up
-    #welcome_to_awesome_quiz() # Calls the welcome function to introduce player etc
-    #topic_selector()
+    welcome_to_awesome_quiz() # Calls the welcome function to introduce player etc
+    topic_selector()
     
     #question_getter(questions) # Not sure what to do with him
 
